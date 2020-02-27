20331f4d9b68c2d220478b7e582e982b3dceed67
unknown <1419528576@qq.com>
2020-02-27 13:24:06 +0800

    用vue实现商品list小练习完成

---
+++ vue.js-study7/produce/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <produceList v-for="item in 10" :key="item"></produceList>
+  </div>
+</template>
+
+<script>
+//引入组件
+import produceList from "./produce-list";
+export default {
+  name: "app",
+  //注册组件
+  components: {
+    produceList: produceList
+  }
+};
+</script>
+
+<style>
+#app {
+  width: 1400px;
+  margin: 50px auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ vue.js-study7/produce/src/produce-list.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="produce">
+    <img
+      src="http://img.alicdn.com/bao/uploaded/i1/711882582/O1CN01LeVZDA1UwbDfMntKX_!!0-item_pic.jpg"
+      alt
+    />
+    <span class="sp1">￥399.99</span>
+    <span class="sp2">dickies连帽卫衣全棉休闲</span>
+    <span class="sp3">dickies官方旗舰店</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.produce {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #000;
+  margin-right: 5px;
+  margin-bottom: 5px;
+}
+.produce img {
+  width: 270px;
+}
+span {
+  display: inline-block;
+  padding: 0 10px;
+}
+.sp1 {
+  color: red;
+  font-size: 20px;
+}
+.sp2 {
+  font-size: 16px;
+}
+.sp3 {
+  color: #ccc;
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
